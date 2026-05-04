e60b5f2b8b1dd473ba9185e5279756a9dbaf2999
Rodrigo Pat <rodrigop4t@gmail.com>
2026-05-04 18:56:33 -0500

    Actualizado Actividad 3.1 y 3.2

--- TemaTres_Lenguajes/Ejercicios_3.1/3.1 API_CRUD/index.js
+++ TemaTres_Lenguajes/Ejercicios_3.1/3.1 API_CRUD/index.js
@@ -32,7 +32,82 @@ app.post('/usuarios', async (req, res) => {
   }
 });
 
-//  CONEXIÓN Y SERVIDOR
+// Seccion 5
+app.get('/usuarios', async (req, res) => {
+  try {
+    const usuarios = await Usuario.find();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    console.error("Error al obtener los usuarios:", error);
+    res.status(500).json({ error: "Error al obtener los usuarios" });
+  }
+});
+
+//  Seccion 6 OBTENER USUARIO POR ID
+app.get('/usuario/:id', async (req, res) => {
+  try {
+    // Extrae el id
+    const { id } = req.params;
+
+    // Busca en la BD
+    const usuario = await Usuario.findById(id);
+
+    // Validación (muy importante)
+    if (!usuario) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    // Respuesta
+    res.status(200).json(usuario);
+
+  } catch (error) {
+    console.error("Error al obtener usuario:", error);
+    res.status(500).json({ error: "Error al obtener usuario" });
+  }
+});
+
+// SECCIÓN 7 (PUT)
+app.put('/usuario/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const usuario = await Usuario.findByIdAndUpdate(id, req.body);
+
+    if (!usuario) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    const usuarioActualizado = await Usuario.findById(id);
+
+    res.status(200).json(usuarioActualizado);
+    console.log(usuarioActualizado);
+
+  } catch (error) {
+    console.error("Error al actualizar usuario:", error);
+    res.status(500).json({ error: "Error al actualizar usuario" });
+  }
+});
+
+// SECCIÓN 8 (DELETE)
+app.delete('/usuario/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const usuario = await Usuario.findByIdAndDelete(id);
+
+    if (!usuario) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    res.status(200).json({ message: "Usuario eliminado" });
+
+  } catch (error) {
+    console.error("Error al eliminar usuario:", error);
+    res.status(500).json({ error: "Error al eliminar usuario" });
+  }
+});
+
+//  CONEXIÓN Y SERVIpDOR
 async function startServer() {
   try {
     await mongoose.connect(uri);
